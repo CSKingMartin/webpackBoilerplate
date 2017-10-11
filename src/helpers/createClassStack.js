12ffc78b74c001a16b801f35c26fcaf4dc40c813
Conner Wells <cwells@connectivedx.com>
2017-10-11 10:35:18 -0700

    added support for a classStack in React components

---
+++ src/helpers/createClassStack.js
@@ -0,0 +1,14 @@
+const createClassStack = (classList) => (
+  classList
+    .map((className) => {
+      if (Array.isArray(className)) {
+        return createClassStack(className);
+      }
+
+      return className;
+    })
+    .filter((a) => a)
+    .join(' ')
+);
+
+module.exports = createClassStack;
